e475af0bb74de07c669d79f97be73433bd9f7e9b
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-29 12:42:46 +0200

    comb

---
+++ printcomb.go
@@ -0,0 +1,18 @@
+package PrintRune
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := 48; i < 58; i++ {
+		for j := i + 1; j < 58; j++ {
+			for k := j + 1; k < 58; k++ {
+				z01.PrintRune(rune(i))
+				z01.PrintRune(rune(j))
+				z01.PrintRune(rune(k))
+				z01.PrintRune(rune(','))
+				z01.PrintRune(rune(' '))
+			}
+		}
+	}	
+
+}
