1279adc9123cfd67dd9c0bbbb518bf6cbe560479
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-29 21:59:24 +0200

    strrev

--- strrev.go
+++ strrev.go
@@ -3,7 +3,7 @@ package piscine
 func StrRev(s string) string {
 	rns := []rune(s)
 	for i := 0; i < len(s)/2; i++ {
-		rns[i],rns[len(s)-1-i] = rns[len(s)-1-i], rns[i]
+		rns[i], rns[len(s)-1-i] = rns[len(s)-1-i], rns[i]
 	}
 	return string(rns)
 }
