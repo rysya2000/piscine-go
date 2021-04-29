3f5599bfaa452896ca2fbeff9d3478222b915fa4
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-29 20:10:00 +0200

    nbr

--- printnbr.go
+++ printnbr.go
@@ -3,26 +3,25 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	var answer string = ""
-	var str string = ""
-	var p int = n
-	if n < 0 {
-		n *= -1
-		answer += "-"
-	}
-	for p > 0 {
-		var k int = (p % 10) + 48
-		str += string(k)
-		p /= 10
-	}
-	for i := 0; i < len(str)/2; i++ {
-		str[i] = str[len(str)-i-1]
-	}
-	for i := len(str) - 1; i >= len(str)/2; i-- {
-		str[i] = str[len(str)-i-1]
-	}
-	answer += str
-	for i := 0; i < len(answer); i++ {
-		z01.PrintRune(rune(answer[i]))
-	}
+    var answer string = ""
+    var str string = ""
+    if n < 0 {
+        z01.PrintRune('-')
+    }
+    for {
+        var k int = (n % 10)
+		if k < 0 {
+			k *= -1
+		}
+        str += string(k + 48)
+        n /= 10
+    }
+    rns := []rune(str)
+    for i := 0; i < len(rns)/2; i++ {
+        rns[i], rns[len(rns)-1-i] = rns[len(rns)-i-1], rns[i]
+    }
+    answer += string(rns)
+    for i := 0; i < len(answer); i++ {
+        z01.PrintRune(rune(answer[i]))
+    }
 }
