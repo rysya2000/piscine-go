198258a93cda551e8ab0a697bf6df525f547f631
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-29 10:55:19 +0200

    rev

---
+++ printreversealphabet/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main(){
+	var a string = "zyxwvutsrqponmlkjihgfedcba"
+	for i := 0; i < 26; i++ {
+		z01.PrintRune(rune(a[i]))
+	}
+	z01.PrintRune('\n')
+}
