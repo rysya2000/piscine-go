d785080bbaf30433f135694d52d7a5257b91a438
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-29 22:57:28 +0200

    atoi

---
+++ basicatoi.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	rns := []rune(s)
+	var ans int = 0
+	k := 1
+	for i := 0; i < len(s); i++ {
+		k *= 10
+	}
+	for i := 0; i < len(s); i++ {
+		ans += (int(rns[i]) - '0') * k
+
+			k /= 10
+	}
+	return ans/10
+}
