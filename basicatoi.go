ad4204a33c6dd8733a9407a8ddbfee0e65d2509f
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-30 12:17:29 +0200

    atoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,10 +1,10 @@
 package piscine
 
 func BasicAtoi(s string) int {
-    	n := 0
-    	for _, ch := range []byte(s) {
-        	ch -= 48
-        	n = n*10 + int(ch)
-    }
-    return int(n)
+	n := 0
+	for _, ch := range []byte(s) {
+		ch -= 48
+		n = n*10 + int(ch)
+	}
+	return int(n)
 }
