43a058d00c9286d7688c73f2bdf01dfd8ccde2f4
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-04-30 14:19:15 +0200

    atoi

---
+++ atoi.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Atoi(s string) int {
+	n := 0
+	for index, ch := range []byte(s) {
+		if index != 0 && (48 > ch || ch > 57) {
+			return 0
+		}
+		if (48 > ch || ch > 57) && ch != '+' && ch != '-'{
+			return 0
+		}
+		if 48 <= ch && ch <= 57 {
+			ch -= 48
+			n = n*10 + int(ch)
+		}
+	}
+	if s[0] == '-' {
+		n = -n
+	}
+	return int(n)
+}
