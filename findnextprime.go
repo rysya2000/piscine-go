826ef0ccfd3f529eede9f52bfb9eb5c113e472f0
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-03 18:22:12 +0200

    findnetprime

--- findnextprime.go
+++ findnextprime.go
@@ -1,15 +1,13 @@
 package piscine
 
-import "math"
-
 func FindNextPrime(nb int) int {
 	if nb < 3 {
 		return 2
 	}
 	for {
 		b := 1
-		for i := 2.0; i <= math.Pow(float64(nb), 0.5); i++ {
-			if nb%int(i) == 0 {
+		for i := 2; i <= nb/2; i++ {
+			if nb%i == 0 {
 				b = 0
 			}
 		}
