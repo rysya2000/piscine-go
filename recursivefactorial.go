dd4cbb16921ae2c0a4481ace107b5e355f7c6f7c
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-03 15:45:13 +0200

    RecFActoirial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
-func IterativeFactorial(nb int) int {
+func RecursiveFactorial(nb int) int {
 	if nb == 0 {
 		return 1
 	}
 	if nb > 0 && nb < 21 {
-		return nb * IterativeFactorial(nb-1)
+		return nb * RecursiveFactorial(nb-1)
 	}
 	return 0
 }
