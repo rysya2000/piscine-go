d09442d198cc31c15234f2ae46995389f4f30ef8
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-03 18:31:47 +0200

    findnetprime

--- findnextprime.go
+++ findnextprime.go
@@ -1,12 +1,12 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb < 3 {
+	if nb < 3 || nb > 2147483647{
 		return 2
 	}
 	for {
 		b := 1
-		for i := 2; i < 2147483647; i++ {
+		for i := 2; i < nb; i++ {
 			if nb%i == 0 {
 				b = 0
 				break
