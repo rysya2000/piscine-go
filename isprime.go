923acf053539a388b4bd22958f0225c7059c0840
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-03 17:32:42 +0200

    isprime

--- isprime.go
+++ isprime.go
@@ -1,17 +1,5 @@
 package piscine
 
-func Sqrt(nb int) int {
-	if nb == 1 {
-		return 1
-	}
-	for i := 1; i <= nb/2; i++ {
-		if i*i == nb {
-			return i
-		}
-	}
-	return 0
-}
-
 func IsPrime(nb int) bool {
 	if nb == 1 {
 		return false
@@ -22,7 +10,7 @@ func IsPrime(nb int) bool {
 	if nb < 0 {
 		return false
 	}
-	for i := 2; i <= Sqrt(nb); i++ {
+	for i := 2; i < nb; i++ {
 		if nb%i == 0 {
 			return false
 		}
