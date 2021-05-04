6463ce5efa7618fc11684ca89c0db71df0ac06e6
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-04 18:25:47 +0200

    alphacount-1

--- alphacount.go
+++ alphacount.go
@@ -2,8 +2,8 @@ package piscine
 
 func AlphaCount(s string) int {
 	cnt := 0
-	for i := 0; i < len(s); i++{
-		if (s[i] >= 65 && s[i] <= 90) || ((s[i] >= 97 && s[i] <= 122)) {
+	for i := 0; i < len(s); i++ {
+		if (s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122) {
 			cnt++
 		}
 	}
