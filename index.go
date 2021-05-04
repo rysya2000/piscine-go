1b2b7304500ad09e25b5d1abcc8183a0c29ebd50
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-04 18:46:41 +0200

    index-1

---
+++ index.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	for i := 0; i < len(s); i++ {
+		b := 1
+		for j := 0; j < len(toFind); j++ {
+			if s[i + j] != toFind[j] && i + j < len(s) {
+				b = 0
+				break
+			}
+		}
+		if b == 1 {
+			return i
+		}
+	}
+	return -1
+}
