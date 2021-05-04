214f6d428a33acfef26e616b4ebea98e7e8f7815
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-04 18:47:15 +0200

    index-2

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ func Index(s string, toFind string) int {
 	for i := 0; i < len(s); i++ {
 		b := 1
 		for j := 0; j < len(toFind); j++ {
-			if s[i + j] != toFind[j] && i + j < len(s) {
+			if s[i+j] != toFind[j] && i+j < len(s) {
 				b = 0
 				break
 			}
