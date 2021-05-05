0217d4e1256adb85b554673cd481f798566a2e4c
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-05 17:56:23 +0200

    printprogramname/main.go-1

---
+++ printprogramname/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	t := []rune(string(os.Args[0]))
+	for i := 2; i < len(t); i++ {
+		z01.PrintRune(t[i])
+	}
+	z01.PrintRune('\n')
+}
