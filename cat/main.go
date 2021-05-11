2d7e284b2f063583beb41f933c8283f75de5e45b
gingeslover <Gabikg21@gmail.com>
2021-05-11 18:20:03 +0200

    cat-1

---
+++ cat/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) > 0 {
+		for i := 0; i < len(args); i++ {
+			content, err := ioutil.ReadFile(args[i])
+			if err != nil {
+				for _, j := range "ERROR:" {
+					z01.PrintRune(rune(j))
+				}
+				io.WriteString(os.Stdout, err.Error())
+				z01.PrintRune('\n')
+				os.Exit(1)
+			} else {
+				io.WriteString(os.Stdout, string(content))
+			}
+		}
+	}
+}
