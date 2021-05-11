8474b7fcb3e0620b6de174c020e445fd56503178
gingeslover <Gabikg21@gmail.com>
2021-05-11 15:14:51 +0200

    point-2

--- point/main.go
+++ point/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 )
+
 type point struct {
 	x int
 	y int
@@ -16,5 +17,18 @@ func setPoint(ptr *point) {
 func main() {
 	points := &point{}
 	setPoint(points)
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('4')
+	z01.PrintRune('2')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('2')
+	z01.PrintRune('1')
+	z01.PrintRune('\n')
 }
