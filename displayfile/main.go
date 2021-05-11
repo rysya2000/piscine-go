5e1d100edb4e68dfc1af94c915295980e63f57a5
gingeslover <Gabikg21@gmail.com>
2021-05-11 15:53:33 +0200

    displayfile-3

--- displayfile/main.go
+++ displayfile/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) == 0 {
-		fmt.Println("file name missing")
+		fmt.Println("File name missing")
 	}
 	if len(args) > 1 {
 		fmt.Println("Too many arguments")
