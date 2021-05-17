8106425bcdd2545a47b2ee7d2701f498d2cecb9e
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-18 00:03:31 +0200

    listclear-1

--- listclear.go
+++ listclear.go
@@ -1,51 +1,5 @@
-package main
-
-import "fmt"
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
-func PrintList(l *List) {
-	link := l.Head
-	for link != nil {
-		fmt.Print(link.Data, " -> ")
-		link = link.Next
-	}
-	fmt.Println(nil)
-}
+package piscine
 
 func ListClear(l *List) {
 	*l = List{}
-}
-
-func main() {
-	link := &List{}
-	ListPushBack(link, "I")
-	ListPushBack(link, 1)
-	ListPushBack(link, "something")
-	ListPushBack(link, 2)
-
-	fmt.Println("------list------")
-	PrintList(link)
-	ListClear(link)
-	fmt.Println("------updated list------")
-	PrintList(link)
-}
-
-func ListPushBack(l *List, data interface{}) {
-	n := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = n
-		l.Tail = n
-	} else {
-		l.Tail.Next = n
-		l.Tail = n
-	}
 }
