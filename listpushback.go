415bfb5a79828dc1328d9e247380eeffce3e8b80
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-17 21:18:10 +0200

    listpushback-1

---
+++ listpushback.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		l.Tail.Next = n
+		l.Tail = n
+	}
+}
+
+func main() {
+
+	link := &List{}
+
+	ListPushBack(link, "Hello")
+	ListPushBack(link, "man")
+	ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
+}
