030fdc6760dbf2ddfe80e8900e3602279e1f7577
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-18 16:43:37 +0200

    listforeach-2

--- listforeach.go
+++ listforeach.go
@@ -1,5 +1,23 @@
 package piscine
 
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
+
 func ListForEach(l *List, f func(*NodeL)) {
 	n := l.Head
 	for n != nil {
