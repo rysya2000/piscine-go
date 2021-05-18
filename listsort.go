0c4bc996984715653d2b4721f0d3d8cf82cb4952
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-18 21:39:26 +0200

    listsort-2

--- listsort.go
+++ listsort.go
@@ -10,9 +10,9 @@ func ListSort(l *NodeI) *NodeI {
 		return l
 	}
 	n := l
-	for ;n != nil; n =n.Next {
+	for ; n != nil; n = n.Next {
 		m := n.Next
-		for ;m != nil;m = m.Next {
+		for ; m != nil; m = m.Next {
 			if n.Data > m.Data {
 				n.Data, m.Data = m.Data, n.Data
 			}
