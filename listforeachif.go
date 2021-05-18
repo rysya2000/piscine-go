b6101e3b54b638d1de841ecf31240898e45a8e62
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-18 17:27:28 +0200

    listforeachif-1

---
+++ listforeachif.go
@@ -0,0 +1,105 @@
+package main
+
+import "fmt"
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func IsPositive_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNegative_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) < 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNotNumeric_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	case string, rune:
+		return true
+	}
+	return true
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	n := l.Head
+	for n != nil {
+		if cond(n) {
+			f(n)
+		}
+		n = n.Next
+	}
+}
+
+func main() {
+	link := &List{}
+
+	ListPushBack(link, 1)
+	ListPushBack(link, "hello")
+	ListPushBack(link, 3)
+	ListPushBack(link, "there")
+	ListPushBack(link, 23)
+	ListPushBack(link, "!")
+	ListPushBack(link, 54)
+
+	PrintList(link)
+
+	fmt.Println("--------function applied--------")
+	ListForEachIf(link, PrintElem, IsPositive_node)
+
+	ListForEachIf(link, StringToInt, IsNotNumeric_node)
+
+	fmt.Println("--------function applied--------")
+	PrintList(link)
+
+	fmt.Println()
+}
+
+func PrintElem(node *NodeL) {
+	fmt.Println(node.Data)
+}
+
+func StringToInt(node *NodeL) {
+	node.Data = 2
+}
+
+func PrintList(l *List) {
+	it := l.Head
+	for it != nil {
+		fmt.Print(it.Data, "->")
+		it = it.Next
+	}
+	fmt.Print("nil","\n")
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		l.Tail.Next = n
+		l.Tail = n
+	}
+}
