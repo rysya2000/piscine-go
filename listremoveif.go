df4d98d9df4daf45813934027e97863a76a97363
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-18 20:28:10 +0200

    listremoveif-2

--- listremoveif.go
+++ listremoveif.go
@@ -1,11 +1,10 @@
 package piscine
 
 func ListRemoveIf(l *List, data_ref interface{}) {
-	n := l.Head
-	for n.Data == data_ref {
+	for l.Head != nil && l.Head.Data == data_ref {
 		l.Head = l.Head.Next
-		n = l.Head
 	}
+	n := l.Head
 	for n != nil && n.Next != nil {
 		if n.Next.Data == data_ref {
 			n.Next = n.Next.Next
