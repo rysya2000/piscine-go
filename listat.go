7c4af5acef6568c3e7fb43706064a8eefdf13914
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-18 14:57:37 +0200

    listat-1

---
+++ listat.go
@@ -0,0 +1,18 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+func ListAt(l *NodeL, pos int) *NodeL{
+	n := 0
+	for l != nil {
+		n++
+		l = l.Next
+		if pos == n {
+			return l.Next
+		}
+	}
+	return nil
+}
