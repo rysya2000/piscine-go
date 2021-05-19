858f041259691997617b835cc373cb93557d7a04
rysya2000 <rysaldy.kayipbergen@gmail.com>
2021-05-19 22:17:49 +0200

    sortedlistmerge-3

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -1,19 +1,22 @@
 package piscine
 
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
-	x := n1
-	for x.Next != nil {
-		x = x.Next
-	}
-	x.Next = n2
-	i := n1
-	for ; i != nil; i = i.Next {
-		j := i.Next
-		for ; j != nil; j = j.Next {
-			if i.Data > j.Data {
-				i.Data, j.Data = j.Data, i.Data
+	if n1 != nil {
+		x := n1
+		for x.Next != nil {
+			x = x.Next
+		}
+		x.Next = n2
+		i := n1
+		for ; i != nil; i = i.Next {
+			j := i.Next
+			for ; j != nil; j = j.Next {
+				if i.Data > j.Data {
+					i.Data, j.Data = j.Data, i.Data
+				}
 			}
 		}
+		return n1
 	}
-	return n1
+	return n2
 }
